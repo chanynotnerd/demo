bf6917969185e7254ab34c38f6d36d1b1a43db5e
chanynotnerd <63177308+chanynotnerd@users.noreply.github.com>
2023-12-05 16:59:52 +0900

    change build.gradle with spring boot version

---
+++ src/main/java/com/ssamz/demo/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.ssamz.demo.controller;
+
+public class UserController {
+
+}
---
+++ src/main/java/com/ssamz/demo/domain/User.java
@@ -0,0 +1,19 @@
+package com.ssamz.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "USERS")
+public class User {
+
+}
